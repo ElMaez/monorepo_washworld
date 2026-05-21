b86219e70c76eb086b60fc9e4e98f1772a9a9b32
ElMaez <mai_ez@outlook.dk>
2026-05-21 15:42:25 +0200

    fonts path var forkert så der kunne ikke buildes

--- frontend/app/fonts/fonts.ts
+++ frontend/app/fonts/fonts.ts
@@ -3,27 +3,27 @@ import localFont from "next/font/local";
 export const gilroy = localFont({
     src: [
         {
-            path: "./fonts/Gilroy-Regular.woff2",
+            path: "./Gilroy-Regular.woff2",
             weight: '400',
             style: 'normal',
         },
         {
-            path: "./fonts/Gilroy-Medium.woff2",
+            path: "./Gilroy-Medium.woff2",
             weight: '500',
             style: 'normal',
         },
         {
-            path: "./fonts/Gilroy-Light.woff2",
+            path: "./Gilroy-Light.woff2",
             weight: '300',
             style: 'normal',
         },
         {
-            path: "./fonts/Gilroy-Bold.woff2",
+            path: "./Gilroy-Bold.woff2",
             weight: '700',
             style: 'normal',
         },
         {
-            path: "./fonts/Gilroy-Heavy.woff2",
+            path: "./Gilroy-Heavy.woff2",
             weight: '800',
             style: 'normal',
         },

1227574aae613ccc7ca3ca1b42fd97ae85c1082c
katelijacobsen <katjamaehleke98@gmail.com>
2026-05-24 10:45:18 +0200

    push

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,30 +1,11 @@
-<<<<<<< HEAD
 import Form from "./features/authentication/components/SignupForm";
-=======
-import Form from "./features/signup/components/Form";
-import Header from "./global/components/Header";
->>>>>>> 6b6c55837d1827460f77fcc85d70af2659540643
 import Input from "./global/components/Input";
 
 export default function Home() {
   return (
-<<<<<<< HEAD
    <main>
     <h1>Hi! I am main!</h1>
-    <Form />
     <p>Css global works! yaie!</p>
    </main>
-=======
-    <>
-    <Header
-    title="Hjem"
-/>
-    <main>
-      <Form />
-      <h1>Hi! I am main!</h1>
-      <p>Css global works! yaie!</p>
-    </main>
-   </>
->>>>>>> 6b6c55837d1827460f77fcc85d70af2659540643
   );
 }
